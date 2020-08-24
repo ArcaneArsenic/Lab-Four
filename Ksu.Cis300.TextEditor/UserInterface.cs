1ed1aeb0edd453f193850cf2c01146bb98a4d0ee
ArcaneArsenic <tclevi@ksu.edu>
2020-08-24 13:43:07 -0500

    b

---
+++ Ksu.Cis300.TextEditor/UserInterface.cs
@@ -0,0 +1,122 @@
+﻿/* UserInterface.cs
+ * Author: Rod Howell
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Ksu.Cis300.TextEditor
+{
+    /// <summary>
+    /// A GUI for a simple text editor.
+    /// </summary>
+    public partial class UserInterface : Form
+    {
+        /// <summary>
+        /// Constructs the GUI.
+        /// </summary>
+        public UserInterface()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Rotates the given character c n positions through the alphabet whose first
+        /// letter is firstLetter and whose number of letters is alphabetLen. alphabetLen
+        /// must be positive.
+        /// </summary>
+        /// <param name="c">The character to rotate.</param>
+        /// <param name="n">The number of positions to rotate c.</param>
+        /// <param name="firstLetter">The first letter of the alphabet.</param>
+        /// <param name="alphabetLen">The number of letters in the alphabet.</param>
+        /// <returns>The result of the rotation.</returns>
+        private char Rotate(char c, int n, char firstLetter, int alphabetLen)
+        {
+            return (char)(firstLetter + (c - firstLetter + n) % alphabetLen);
+        }
+
+        private char Encrypt(char letter)
+        {
+            if (letter <= 'z') { return Rotate(letter, 13, 'a', 26); }
+            else if (letter > 'z') { return Rotate(letter, 13, 'A', 26); }
+            else { return letter; }
+        }
+
+        /// <summary>
+        /// Handles a Click event on the "Open . . ." file menu item.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void uxOpen_Click(object sender, EventArgs e)
+        {
+            if (uxOpenDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    uxEditBuffer.Text = File.ReadAllText(uxOpenDialog.FileName);
+                }
+                catch (Exception ex)
+                {
+                    ShowError(ex);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Handles a Click event on the "Save As . . ." file menu item.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void uxSaveAs_Click(object sender, EventArgs e)
+        {
+            if (uxSaveDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    File.WriteAllText(uxSaveDialog.FileName, uxEditBuffer.Text);
+                }
+                catch (Exception ex)
+                {
+                    ShowError(ex);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Displays the given exception to the user.
+        /// </summary>
+        /// <param name="e">The exception to show.</param>
+        private void ShowError(Exception e)
+        {
+            MessageBox.Show("The following error occurred: " + e);
+        }
+
+        private void uxStringTSMI_Click(object sender, EventArgs e)
+        {
+            string result = "";
+
+            foreach (char x in uxEditBuffer.Text)
+            {
+                result += Encrypt(x);
+            }
+            uxEditBuffer.Text = result;
+        }
+
+        private void uxStringBuilderTSMI_Click(object sender, EventArgs e)
+        {
+            StringBuilder result = new StringBuilder();
+
+            foreach (char x in uxEditBuffer.Text)
+            {
+                result.Append(x);
+            uxEditBuffer.Text = Convert.ToString(result);
+        }
+    }
+}
